d2e9c6fbb07718c89f3236afcac4cef012fe8ee8
kensukekoba <kensukekoba@gmail.com>
2016-07-07 23:20:58 +0000

    nav-tab sort metaphors.count

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,7 +1,7 @@
 class TopPagesController < ApplicationController
   def home
     @topics = Topic.all.order(created_at: :desc)
-    @topics_rank = Topic.all.order(created_at: :asc)
+    @topics_rank = Metaphor.group(:topic_id).order('count_all desc').limit(15).count.keys.map{|id| Topic.find(id)}
   end
 
 end
