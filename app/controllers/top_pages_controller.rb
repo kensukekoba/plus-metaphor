dc389bbf8342c6bac76d2b3ac2d52ad7319db3c0
kensukekoba <kensukekoba@gmail.com>
2016-07-22 11:25:07 +0000

    home officialcontent

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -2,6 +2,7 @@ class TopPagesController < ApplicationController
   def home
     @topics = Topic.all.order(created_at: :desc)
     @topics_rank = Metaphor.group(:topic_id).order('count_all desc').limit(15).count.keys.map{|id| Topic.find(id)}
+    @topics_official = Topic.where(officialflag: true).order(created_at: :desc)
   end
 
 end
