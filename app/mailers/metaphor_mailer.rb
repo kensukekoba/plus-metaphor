5a1b86b3329f6e2585000ce00fc7e1924155c16e
kensukekoba <kensukekoba@gmail.com>
2016-07-23 01:25:17 +0000

    about text

--- app/mailers/metaphor_mailer.rb
+++ app/mailers/metaphor_mailer.rb
@@ -3,6 +3,6 @@ class MetaphorMailer < ApplicationMailer
   def add_metaphor(user)
     @user = user
     @url = 'https://techacademy-kensukekoba.c9users.io/login'
-    mail(to: @user.email, subject: 'create new metaphor!')
+    mail(to: @user.email, subject: '新たな比喩が追加されました!')
   end
 end
